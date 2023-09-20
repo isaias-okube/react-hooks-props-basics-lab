3d287dabe19b6a650cce00b417bdd98d92ff2fa2
ISAIAS <issuyep@yahoo.com>
2023-09-20 02:33:06 -0600

    complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,19 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About({ github, linkedin, bio}) {
+  if (bio && bio.trim() !== '') {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {<Links github={github} linkedin={linkedin} bio={bio} />}
     </div>
   );
+  } else {
+    return null;
+  }
 }
 
 export default About;
